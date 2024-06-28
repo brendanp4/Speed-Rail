d26475253c2996486f4108299008ac824e03f1ae
brendanp4 <brendanp4041@gmail.com>
2024-06-28 10:18:46 -0400

    Add speed rail item

--- src/client/java/net/bossman/speedrail/SpeedRailClient.java
+++ src/client/java/net/bossman/speedrail/SpeedRailClient.java
@@ -1,10 +1,11 @@
 package net.bossman.speedrail;
 
+import net.bossman.speedrail.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 
 public class SpeedRailClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ModItems.RegisterModItems();
 	}
 }
