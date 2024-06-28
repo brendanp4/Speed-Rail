ddbdcd594c17e40b9c299398b294f69dcd8e173a
brendanp4 <brendanp4041@gmail.com>
2024-06-28 08:53:12 -0400

    Initial commit

---
+++ src/client/java/net/bossman/speedrail/SpeedRailClient.java
@@ -0,0 +1,10 @@
+package net.bossman.speedrail;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpeedRailClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/bossman/speedrail/SpeedRailDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bossman.speedrail;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpeedRailDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
